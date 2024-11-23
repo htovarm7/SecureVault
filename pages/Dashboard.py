c94e1d0570029a48706e7a03dba34cad1cd695d9
Héctor Tovar <h.tovarm07@gmail.com>
2024-11-22 22:11:16 -0600

    dashboard ready!

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -2,4 +2,4 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 looker_studio_url = "https://lookerstudio.google.com/embed/reporting/2fa34e9b-b39c-47a4-a2df-f2d99be5919c/page/ZL8RE"
-components.iframe(looker_studio_url, width=800, height=600) 
+components.iframe(looker_studio_url, width=1080, height=1484) 
