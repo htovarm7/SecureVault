2f6449f7e97708e26cd8e68793088e66cb774d6a
Héctor Tovar <h.tovarm07@gmail.com>
2024-11-22 22:08:49 -0600

    se configura bien la seccion de paginas

--- Main.py
+++ Main.py
@@ -4,12 +4,10 @@ pages = {
     ""
     "Secure Vault 🔒": [
         st.Page("pages/Sobre_Nosotros.py", title="Sobre Nosotros 👥"),
-        st.Page("pages/Contacto.py", title="Contactanos 📩"),
-    ],
-    "Proyecto": [
         st.Page("pages/Prototipo.py", title="Prototipo 🚀"),
         st.Page("pages/Dashboard.py", title="Dashboard 📊"),
         st.Page("pages/Reconocimiento.py", title="Reconocimiento Facial👤"),
+        st.Page("pages/Contacto.py", title="Contactanos 📩"),
     ],
 }
 
