cbecbdc661dcc4011b70672245f17885e53ea798
Héctor Tovar <h.tovarm07@gmail.com>
2024-11-22 22:12:34 -0600

    dashboard finish!

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+st.title("Dashboard 📊")
+st.header("Visualziación de los datos en Looker Studio")
+
 looker_studio_url = "https://lookerstudio.google.com/embed/reporting/2fa34e9b-b39c-47a4-a2df-f2d99be5919c/page/ZL8RE"
 components.iframe(looker_studio_url, width=1080, height=1080) 
